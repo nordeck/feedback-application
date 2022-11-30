0e8089199ede6e56dfb2a030356e5d7aa0c0cfe7
nikolas.mirillas@nordeck.net <nikolas.mirillas@nordeck.net>
2022-11-30 11:29:54 +0100

    return err rather than log it

--- backend/internal/client/rest_client.go
+++ backend/internal/client/rest_client.go
@@ -2,17 +2,14 @@ package client
 
 import (
 	"bytes"
-	"feedback/internal/logger"
 	"io"
 	"net/http"
 )
 
-var log = logger.Instance()
-
 func Post(url string, reqBody []byte) (io.ReadCloser, error) {
 	resp, err := http.Post(url, "application/json", bytes.NewBuffer(reqBody))
 	if err != nil {
-		log.Error(err)
+		return nil, err
 	}
 	return resp.Body, err
 }
