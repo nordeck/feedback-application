b1051b2354c8a9ac291bfa0ccb281dea10b2dd9e
nikolas.mirillas@nordeck.net <nikolas.mirillas@nordeck.net>
2022-12-07 13:03:51 +0100

    fix typo
    Signed-off-by: nikolas.mirillas@nordeck.net <nikolas.mirillas@nordeck.net>

--- backend/internal/client/rest_client.go
+++ backend/internal/client/rest_client.go
@@ -10,7 +10,7 @@ import (
 func Post(config *internal.Configuration, reqBody []byte) (io.ReadCloser, error) {
 	client := &http.Client{}
 	req, err := http.NewRequest("POST", config.OidcValidationUrl, bytes.NewBuffer(reqBody))
-	req.Header.Add("Authentication", "Bearer "+config.UvsAuthToken)
+	req.Header.Add("Authorization", "Bearer "+config.UvsAuthToken)
 	resp, err := client.Do(req)
 	if err != nil {
 		return nil, err
